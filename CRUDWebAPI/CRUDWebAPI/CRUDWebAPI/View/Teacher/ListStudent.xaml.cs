7086a29256b65d66f910376c9c58eb0fb44f4f7f
anhkhang <anhkhang110@gmail.com>
2021-01-23 20:43:45 +0700

    add teacher folder

---
+++ CRUDWebAPI/CRUDWebAPI/CRUDWebAPI/View/Teacher/ListStudent.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CRUDWebAPI.View.Teacher
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListStudent : ContentPage
+    {
+        public ListStudent()
+        {
+            InitializeComponent();
+        }
+    }
+}
